bf5462874677f46cfbb76d5b540dc4adbd15187b
emmanueldavids <emmanueldavids417@gmail.com>
2023-08-07 12:03:56 +0000

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
 
 def canUnlockAll(boxes):
-    n = len(boxes)
-    visited = set()
-    stack = [0]  # Start with box 0, which is already unlocked
+    """
+    Function that determines if you can open all the lockboxes
+    Args:
+        boxes: list of lists of integers
+    Returns:
+        True if you can open all the lockboxes, False otherwise
+    """
+    unlocked = set()
 
-    while stack:
-        box = stack.pop()
-        visited.add(box)
-
-        for key in boxes[box]:
-            if key not in visited and key < n:
-                stack.append(key)
-
-    return len(visited) == n
-
-    boxes = [[1], [2], [3], [4], []]
-    print(canUnlockAll(boxes))  # Output: True
-
-    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(canUnlockAll(boxes))  # Output: True
-
-    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes))  # Output: False
+    for box_id, box in enumerate(boxes):
+        if len(box) == 0 or box_id == 0:
+            unlocked.add(box_id)
+        for key in box:
+            if key < len(boxes) and key != box_id:
+                unlocked.add(key)
+        if len(unlocked) == len(boxes):
+            return True
+    return False
