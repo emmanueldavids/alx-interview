3340ead0a69a619030f492c9da47df577fc8b479
emmanueldavids <emmanueldavids417@gmail.com>
2023-08-21 14:43:57 +0000

    script that reads stdin line by line and computes metrics

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,10 +5,12 @@ log parsing
 
 import sys
 
+
 def print_stats(total_size, status_counts):
     print("File size: {}".format(total_size))
     for status_code in sorted(status_counts.keys()):
         print("{}: {}".format(status_code, status_counts[status_code]))
+
 
 total_size = 0
 status_counts = {}
@@ -20,16 +22,17 @@ try:
         if len(parts) >= 7:
             status_code = parts[-2]
             file_size = int(parts[-1])
-            
+
             total_size += file_size
-            
+
             if status_code.isdigit():
                 status_code = int(status_code)
-                status_counts[status_code] = status_counts.get(status_code, 0) + 1
-            
+                status_counts[status_code] =
+                status_counts.get(status_code, 0) + 1
+
             if line_number % 10 == 0:
                 print_stats(total_size, status_counts)
-            
+
 except KeyboardInterrupt:
     pass
 
