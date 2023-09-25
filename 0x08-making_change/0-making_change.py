e4e4ce6fd7f1194a3277cb02d9d4e60922cb6233
emmanueldavids <emmanueldavids417@gmail.com>
2023-09-25 12:05:24 +0000

    changes comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,24 +1,36 @@
 #!/usr/bin/python3
+
+""" Given a pile of coins of different values, determine the
+    fewest number of coins needed to meet a given amount total.
+"""
+
+
 def makeChange(coins, total):
+    """ Prototype: def makeChange(coins, total)
+        Return: fewest number of coins needed to meet total
+        If total is 0 or less, return 0
+        If total cannot be met by any number of coins you have,
+        return -1 coins is a list of the values of the coins in
+        your possession.The value of a coin will always be an integer
+        greater than 0. You can assume you have an infinite number of
+        each denomination of coin in the list. Your solution’s runtime
+        will be evaluated in this task.
+    """
     if total <= 0:
         return 0
 
-    dp = [total + 1] * (total + 1)
+    if (coins is None or len(coins) == 0):
+        return -1
 
-    # Base case: You need 0 coins to make a total of 0.
-    dp[0] = 0
+    change = 0
+    available_coins = sorted(coins, reverse=True)
+    change_left = total
 
-    # Iterate through all possible amounts from 1 to 'total'.
-    for amount in range(1, total + 1):
-        # Try each coin denomination.
-        for coin in coins:
-            if amount - coin >= 0:
-                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+    for coin in available_coins:
+        while (change_left % coin >= 0 and change_left >= coin):
+            change += int(change_left / coin)
+            change_left = change_left % coin
 
-    return dp[total] if dp[total] <= total else -1
+    change = change if change_left == 0 else -1
 
-# Test cases
-
-
-print(makeChange([1, 2, 25], 37))  # Output: 7
-print(makeChange([1256, 54, 48, 16, 102], 1453))  # Output: -1
+    return change
