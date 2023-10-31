efb3b4e1b5901c8ab501082d7c93278e3a7d031b
emmanueldavids <emmanueldavids417@gmail.com>
2023-10-31 22:38:49 +0000

    added files

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,94 +1,50 @@
 #!/usr/bin/python3
+""" N queens puzzle, challenge of placing N non attacking queens
+on a NxN chessboard
+This program solves the N queens problem """
 
-""" The N queens puzzle is the challenge of placing N
-    non-attacking queens on an NxN
-    chessboard. Write a program that solves the N queens problem.
-"""
-
-import sys
+from sys import argv
 
 
-def is_safe(board, row, col):
-    """
-    Checks if it is safe to place a queen at (row, col) on the board.
-
-    Args:
-        board: The board.
-        row: The row index.
-        col: The column index.
-
-    Returns:
-        True if it is safe to place a queen at (row, col), False otherwise.
-    """
-
-    for i in range(row):
-
-        if board[i][col] == 1:
-            return False
-
-    for i in range(row):
-        if board[i][col - row + i] == 1:
-            return False
-
-    for i in range(row):
-        if board[i][col + row - i] == 1:
+def is_NQueen(cell: list) -> bool:
+    """ False if not N Queen, True if N Queen """
+    row_number = len(cell) - 1
+    difference = 0
+    for index in range(0, row_number):
+        difference = cell[index] - cell[row_number]
+        if difference < 0:
+            difference *= -1
+        if difference == 0 or difference == row_number - index:
             return False
     return True
 
 
-def solve_nqueens(board, n):
-    """
-    Solves the N queens problem for a board of size n.
-
-    Args:
-        board: The board.
-        n: The size of the board.
-
-    Returns:
-        True if the problem was solved, False otherwise.
-  """
-
-    if n == 0:
-        return True
-
-    for col in range(n):
-        if is_safe(board, n - 1, col):
-            board[n - 1][col] = 1
-        if solve_nqueens(board, n - 1):
-            return True
-            board[n - 1][col] = 0
-
-    return False
-
-
-def main():
-
-    """
-    The main function.
-    """
-
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
-    try:
-        n = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-    board = [[0 for i in range(n)] for j in range(n)]
-
-    if solve_nqueens(board, n):
-        for i in range(n):
-            print(*board[i])
+def solve_NQueens(dimension: int, row: int, cell: list, output: list):
+    """ Return result of N Queens recursively """
+    if row == dimension:
+        print(output)
     else:
-        print("No solution")
+        for column in range(0, dimension):
+            cell.append(column)
+            output.append([row, column])
+            if (is_NQueen(cell)):
+                solve_NQueens(dimension, row + 1, cell, output)
+            cell.pop()
+            output.pop()
 
 
-if __name__ == "__main__":
-    main()
+if len(argv) != 2:
+    print('Usage: nqueens N')
+    exit(1)
+try:
+    N = int(argv[1])
+except BaseException:
+    print('N must be a number')
+    exit(1)
+if N < 4:
+    print('N must be at least 4')
+    exit(1)
+else:
+    output = []
+    cell = 0
+    solve_NQueens(int(N), cell, [], output)
